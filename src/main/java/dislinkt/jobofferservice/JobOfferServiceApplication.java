dc8cd499a69ca5d7f7a4754e348c0dc13c7adb80
verak13 <verakovacevic98@gmail.com>
2022-07-31 17:12:42 +0200

    feat: app initialization

---
+++ src/main/java/dislinkt/jobofferservice/JobOfferServiceApplication.java
@@ -0,0 +1,13 @@
+package dislinkt.jobofferservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobOfferServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobOfferServiceApplication.class, args);
+	}
+
+}
